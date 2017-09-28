f70702eff15b9849f6ca88f6b0b449bc192a8a3a
Sergei Grebnov <Sergei.Grebnov@gmail.com>
2017-09-28 14:29:56 +0000

    Initial version

---
+++ Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    // // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(ConfigurationManager.AppSettings["LuisAppId"], ConfigurationManager.AppSettings["LuisAPIKey"])))
+        {
+        }
+
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached the none intent. You said: {result.Query}"); //
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("HomeAutomation.TurnOn")]
+        public async Task MyIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached the TurnOn intent. You said: {result.Query}"); //
+            context.Wait(MessageReceived);
+        }
+        
+        [LuisIntent("HomeAutomation.TurnOff")]
+        public async Task MyIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"You have reached the TurnOff intent. You said: {result.Query}"); //
+            context.Wait(MessageReceived);
+        }
+    }
+}
